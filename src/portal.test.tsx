af53b059ace884a1eae5decf0deb607dc63f68fe
Nikolay <nikolay.papakha@gmail.com>
2020-07-21 01:19:47 +0300

    Clean up

--- src/portal.test.tsx
+++ src/portal.test.tsx
@@ -23,7 +23,6 @@ describe('portal.tsx', () => {
       }
 
       public render() {
-        const { containerId, zIndex } = this.state;
         return (
           <Stage ref={ref => (this.stageRef = ref)}>
             <Layer id="layer-slot-1">
@@ -32,7 +31,7 @@ describe('portal.tsx', () => {
               </Group>
             </Layer>
             <Layer id="layer-origin">
-              <Portal containerId={containerId} zIndex={zIndex}>
+              <Portal containerId={this.state.containerId} zIndex={this.state.zIndex}>
                 <Text name="x-content" />
               </Portal>
             </Layer>
@@ -48,14 +47,14 @@ describe('portal.tsx', () => {
 
     function assertNotFound(stage?: Konva.Stage, ...ids: string[]) {
       for (const id of ids) {
-        const container = findFirst<Konva.Container>(stage, id);
+        const container = findFirst<Konva.Container>(stage, '#' + id);
         const textNodes = container?.children.toArray().filter(node => node.nodeType === 'Text');
         expect(textNodes.length).toEqual(0);
       }
     }
 
     function assertContains(stage?: Konva.Stage, id?: string) {
-      const container = findFirst<Konva.Container>(stage, id);
+      const container = findFirst<Konva.Container>(stage, '#' + id);
       const textNodes = container?.find('Text');
       expect(textNodes.length).toEqual(1);
       expect(textNodes[0].name()).toEqual('x-content');
@@ -66,54 +65,54 @@ describe('portal.tsx', () => {
       const stage = wrapper.instance().stageRef?.getStage();
 
       // origin is empty
-      assertNotFound(stage, '#layer-origin');
+      assertNotFound(stage, 'layer-origin');
       // slots are empty
-      assertNotFound(stage, '#layer-slot-1', '#layer-slot-2', '#group-slot-1', '#group-slot-2');
+      assertNotFound(stage, 'layer-slot-1', 'layer-slot-2', 'group-slot-1', 'group-slot-2');
       // by default content is rendered in portal layer of a Stage
-      assertContains(stage, '#' + PORTAL_LAYER_ID);
+      assertContains(stage, PORTAL_LAYER_ID);
     });
 
     it('should teleport into origin', () => {
       const wrapper = mount<App>(<App containerId="layer-origin" />);
       const stage = wrapper.instance().stageRef?.getStage();
 
-      assertNotFound(stage, '#layer-slot-1', '#layer-slot-2', '#group-slot-1', '#group-slot-2');
-      assertNotFound(stage, '#' + PORTAL_LAYER_ID);
+      assertNotFound(stage, 'layer-slot-1', 'layer-slot-2', 'group-slot-1', 'group-slot-2');
+      assertNotFound(stage, PORTAL_LAYER_ID);
 
-      assertContains(stage, '#layer-origin');
+      assertContains(stage, 'layer-origin');
     });
 
     it('should teleport into layer', () => {
       const wrapper = mount<App>(<App containerId="layer-slot-2" />);
       const stage = wrapper.instance().stageRef?.getStage();
 
-      assertNotFound(stage, '#layer-origin');
-      assertNotFound(stage, '#layer-slot-1', '#group-slot-1', '#group-slot-2');
-      assertNotFound(stage, '#' + PORTAL_LAYER_ID);
+      assertNotFound(stage, 'layer-origin');
+      assertNotFound(stage, 'layer-slot-1', 'group-slot-1', 'group-slot-2');
+      assertNotFound(stage, PORTAL_LAYER_ID);
 
-      assertContains(stage, '#layer-slot-2');
+      assertContains(stage, 'layer-slot-2');
     });
 
     it('should teleport into group', () => {
       const wrapper = mount<App>(<App containerId="group-slot-1" />);
       const stage = wrapper.instance().stageRef?.getStage();
 
-      assertNotFound(stage, '#layer-origin');
-      assertNotFound(stage, '#layer-slot-1', '#layer-slot-2', '#group-slot-2');
-      assertNotFound(stage, '#' + PORTAL_LAYER_ID);
+      assertNotFound(stage, 'layer-origin');
+      assertNotFound(stage, 'layer-slot-1', 'layer-slot-2', 'group-slot-2');
+      assertNotFound(stage, PORTAL_LAYER_ID);
 
-      assertContains(stage, '#group-slot-1');
+      assertContains(stage, 'group-slot-1');
     });
 
     it('should teleport into nested group', () => {
       const wrapper = mount<App>(<App containerId="group-slot-2" />);
       const stage = wrapper.instance().stageRef?.getStage();
 
-      assertNotFound(stage, '#layer-origin');
-      assertNotFound(stage, '#layer-slot-1', '#layer-slot-2', '#group-slot-1');
-      assertNotFound(stage, '#' + PORTAL_LAYER_ID);
+      assertNotFound(stage, 'layer-origin');
+      assertNotFound(stage, 'layer-slot-1', 'layer-slot-2', 'group-slot-1');
+      assertNotFound(stage, PORTAL_LAYER_ID);
 
-      assertContains(stage, '#group-slot-2');
+      assertContains(stage, 'group-slot-2');
     });
 
     it('should handle contentId change', () => {
@@ -121,22 +120,22 @@ describe('portal.tsx', () => {
       const instance = wrapper.instance();
       const stage = instance.stageRef?.getStage();
 
-      assertContains(stage, '#group-slot-2');
+      assertContains(stage, 'group-slot-2');
 
       instance.containerId = 'layer-slot-1'; // move to 'layer-slot-1'
 
-      assertContains(stage, '#layer-slot-1'); // found
+      assertContains(stage, 'layer-slot-1'); // found
 
-      assertNotFound(stage, '#layer-origin');
-      assertNotFound(stage, '#layer-slot-1', '#group-slot-1', '#group-slot-2');
-      assertNotFound(stage, '#' + PORTAL_LAYER_ID);
+      assertNotFound(stage, 'layer-origin');
+      assertNotFound(stage, 'layer-slot-1', 'group-slot-1', 'group-slot-2');
+      assertNotFound(stage, PORTAL_LAYER_ID);
 
       instance.containerId = undefined; // move to default portals layer
 
-      assertNotFound(stage, '#layer-origin');
-      assertNotFound(stage, '#layer-slot-1', '#layer-slot-2', '#group-slot-1', '#group-slot-2');
+      assertNotFound(stage, 'layer-origin');
+      assertNotFound(stage, 'layer-slot-1', 'layer-slot-2', 'group-slot-1', 'group-slot-2');
 
-      assertContains(stage, '#' + PORTAL_LAYER_ID); // found
+      assertContains(stage, PORTAL_LAYER_ID); // found
     });
   });
 
@@ -149,15 +148,14 @@ describe('portal.tsx', () => {
       public moveB = (to: PortalProps) => this.setState(from => ({ ...from, b: { ...from.b, ...to } }));
 
       public render() {
-        const { a, b } = this.state;
         return (
           <Stage ref={ref => (this.stageRef = ref)}>
             <Layer id="layer-slot" />
             <Layer id="layer-origin">
-              <Portal containerId={a.containerId} zIndex={a.zIndex}>
+              <Portal {...this.state.a}>
                 <Text name="A" />
               </Portal>
-              <Portal containerId={b.containerId} zIndex={b.zIndex}>
+              <Portal {...this.state.b}>
                 <Text name="B" />
               </Portal>
             </Layer>
@@ -166,13 +164,13 @@ describe('portal.tsx', () => {
       }
     }
 
-    function assertEmpty(stage: Konva.Stage, query: string) {
-      const container = stage.find(query)[0];
+    function assertEmpty(stage: Konva.Stage, id: string) {
+      const container = stage.find('#' + id)[0];
       expect(container?.children.length).toEqual(0);
     }
 
-    function assertOrder(stage: Konva.Stage, query: string, ...ordered: string[]) {
-      const container = stage?.find(query)[0] as Konva.Container;
+    function assertOrder(stage: Konva.Stage, id: string, ...ordered: string[]) {
+      const container = stage?.find('#' + id)[0] as Konva.Container;
       const children = container?.children;
       expect(children.length).toEqual(ordered.length);
       ordered.forEach((content, index) => expect(children[index].name()).toEqual(content));
@@ -188,23 +186,23 @@ describe('portal.tsx', () => {
       const instance = wrapper.instance();
       const stage = instance.stageRef?.getStage() as Konva.Stage;
 
-      assertOrder(stage, '#layer-slot', 'A', 'B'); // natural order
-      assertEmpty(stage, '#layer-origin');
+      assertOrder(stage, 'layer-slot', 'A', 'B'); // natural order
+      assertEmpty(stage, 'layer-origin');
 
       instance.moveA({ zIndex: 4 });
 
-      assertOrder(stage, '#layer-slot', 'B', 'A');
-      assertEmpty(stage, '#layer-origin');
+      assertOrder(stage, 'layer-slot', 'B', 'A');
+      assertEmpty(stage, 'layer-origin');
 
       instance.moveA({ zIndex: -10 });
 
-      assertOrder(stage, '#layer-slot', 'A', 'B');
-      assertEmpty(stage, '#layer-origin');
+      assertOrder(stage, 'layer-slot', 'A', 'B');
+      assertEmpty(stage, 'layer-origin');
 
       instance.moveB({ zIndex: -1000 });
 
-      assertOrder(stage, '#layer-slot', 'B', 'A');
-      assertEmpty(stage, '#layer-origin');
+      assertOrder(stage, 'layer-slot', 'B', 'A');
+      assertEmpty(stage, 'layer-origin');
     });
 
     it('when mounted in specific order', () => {
@@ -213,13 +211,13 @@ describe('portal.tsx', () => {
       const instance = wrapper.instance();
       const stage = instance.stageRef?.getStage() as Konva.Stage;
 
-      assertOrder(stage, '#layer-slot', 'B', 'A'); // mount order
-      assertEmpty(stage, '#layer-origin');
+      assertOrder(stage, 'layer-slot', 'B', 'A'); // mount order
+      assertEmpty(stage, 'layer-origin');
 
       instance.moveB({ zIndex: 54 });
 
-      assertOrder(stage, '#layer-slot', 'B', 'A');
-      assertEmpty(stage, '#layer-origin');
+      assertOrder(stage, 'layer-slot', 'B', 'A');
+      assertEmpty(stage, 'layer-origin');
     });
   });
 
