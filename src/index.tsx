5cb7131efc669e420b5e52e35515274c61cf2a8a
Nikolay <nikolay.papakha@gmail.com>
2020-07-21 23:20:57 +0300

    Fix naming

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { Group as KonvaGroup } from 'konva/types/Group';
 import { Layer as KonvaLayer, LayerConfig } from 'konva/types/Layer';
 import { NodeConfig } from 'konva/types/Node';
-import { Group as ReactKonvaGroup, Layer as LayerComp } from 'react-konva';
+import { Group as ReactKonvaGroup, Layer as ReactKonvaLayer } from 'react-konva';
 import createPortalContainer from './create-container';
 
 export { default as Portal } from './portal';
 export { default as Stage } from './portal-stage';
 
 export const Group = createPortalContainer<KonvaGroup, NodeConfig, typeof ReactKonvaGroup>(ReactKonvaGroup);
-export const Layer = createPortalContainer<KonvaLayer, LayerConfig, typeof LayerComp>(LayerComp);
+export const Layer = createPortalContainer<KonvaLayer, LayerConfig, typeof ReactKonvaLayer>(ReactKonvaLayer);
 
 export { PORTAL_LAYER_ID, Z_INDEX } from './utils';
